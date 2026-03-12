92f6e9ee0233baee3e1f9dc491103d53f8cbef17
lyl0428l <1975104622@qq.com>
2026-03-12 17:18:19 +0800

    cleanup: remove unrelated files, add root .gitignore

--- news_crawler/fix_autostart.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-echo Setting service to auto start (requires admin)...
-powershell -Command "Start-Process powershell.exe -ArgumentList '-ExecutionPolicy Bypass -File \"%~dp0fix_autostart.ps1\"' -Verb RunAs -Wait"
-echo.
-echo Checking result...
-sc qc NewsCrawlerService 2>nul | findstr START_TYPE
-echo.
-pause
